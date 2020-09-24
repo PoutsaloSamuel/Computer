dca859a187b3ee2b4c54aa2addee27d5f1006ee4
Samuel Poutsalo <samuel.poutsalo@myy.haaga-helia.fi>
2020-09-24 19:08:23 +0300

    kolmas lisäys

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,14 +1,14 @@
 package hh.swd20.Bookstore.web;
 
-//import java.util.List;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-//import hh.swd20.Bookstore.domain.Book;
+import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
 
 
@@ -18,10 +18,10 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 
-	@RequestMapping(value="/booklist")
+	@RequestMapping(value="/booklist", method = RequestMethod.GET)
 	public String getBooks(Model model) {
-		//List<Book> books =  (List<Book>) repository.findAll();
-		model.addAttribute("books", repository.findAll());
+		List<Book> books =  (List<Book>) repository.findAll();
+		model.addAttribute("books", books);
 		
 		return "bookhtml";
 	}
