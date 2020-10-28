82fdf3877a89220b9e6d5556a4fea8fb096b5e6d
Samuel Poutsalo <samuel.poutsalo@myy.haaga-helia.fi>
2020-10-28 21:54:19 +0200

    8. lisäys

--- src/test/java/hh/swd20/Bookstore/BookstoreApplicationTests.java
+++ src/test/java/hh/swd20/Bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,24 @@
 package hh.swd20.Bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import hh.swd20.Bookstore.web.BookController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreApplicationTests {
+	
+	@Autowired
+	private BookController bookController;
 
 	@Test
 	void contextLoads() {
+		assertThat(bookController).isNotNull();
 	}
 
 }
